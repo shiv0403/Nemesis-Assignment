692cda9ce15e30f9712e965f383508e2c32e5376
Shivansh Gupta <shiv0403gupta@gmail.com>
2022-02-01 01:54:33 +0530

    commented cors

--- server.js
+++ server.js
@@ -23,18 +23,18 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use(helmet());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  const allowedOrigins = ["http://localhost:3000"];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", true);
-  return next();
-});
+// app.use((req, res, next) => {
+//   const allowedOrigins = ["http://localhost:3000"];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//   }
+//
+//   res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   return next();
+// });
 
 const authRoutes = require("./routes/authrRoutes");
 const dataRoutes = require("./routes/dataRoutes");
